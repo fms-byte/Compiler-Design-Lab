3e18dd57f8f6c39a98157c1a610b06babcc7b247
Farhan-FMS <61820314+fms-byte@users.noreply.github.com>
2024-01-28 20:42:19 +0600

    added

---
+++ Final/input.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    string name = "Farhan";
+    string id = "B190305043";
+    int number = 10;
+    double value = 1.67;
+    double exp = 10e-3;
+    printf("The number is: %d\n", number);
+    return 0;
+}
+// single line comment
+/*
+this is a multiline comment
+*/
